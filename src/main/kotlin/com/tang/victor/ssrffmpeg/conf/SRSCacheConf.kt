8da8cced675872998c6b7c231e1b6fbfa34e4c54
Victor <tonyyunhan1997@163.com>
2023-01-26 17:45:43 +0800

    update

--- src/main/kotlin/com/tang/victor/ssrffmpeg/conf/SRSCacheConf.kt
+++ src/main/kotlin/com/tang/victor/ssrffmpeg/conf/SRSCacheConf.kt
@@ -2,25 +2,17 @@ package com.tang.victor.ssrffmpeg.conf
 
 import com.google.common.cache.Cache
 import com.google.common.cache.CacheBuilder
-import com.google.common.util.concurrent.ThreadFactoryBuilder
-import com.tang.victor.ssrffmpeg.service.SRSValue
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 @Configuration
 class SRSCacheConf {
-
     @Bean
-    fun srsCache(): Cache<String, SRSValue> {
-        return CacheBuilder.newBuilder().build()
-    }
-
-    @Bean
-    fun processSrsThreadPoolExecutorService(): ExecutorService {
-        val threadFactory = ThreadFactoryBuilder()
-                .setNameFormat("srs-thread-%d").build();
-        return Executors.newCachedThreadPool(threadFactory)
+    fun srsCache(): Cache<String, Process> {
+        return CacheBuilder.newBuilder()
+            .removalListener<String, Process> {
+                it.value?.destroy()
+            }
+            .build()
     }
 }
--- src/main/kotlin/com/tang/victor/ssrffmpeg/service/SRSService.kt
+++ src/main/kotlin/com/tang/victor/ssrffmpeg/service/SRSService.kt
@@ -3,60 +3,40 @@ package com.tang.victor.ssrffmpeg.service
 import com.google.common.cache.Cache
 import com.tang.victor.ssrffmpeg.conf.ApiConfiguration
 import com.tang.victor.ssrffmpeg.controller.SRSCallbackBody
+import jakarta.annotation.PreDestroy
 import mu.KotlinLogging
-import org.bytedeco.ffmpeg.global.avcodec.AV_CODEC_ID_AAC
-import org.bytedeco.ffmpeg.global.avcodec.AV_CODEC_ID_H264
-import org.bytedeco.javacv.FFmpegFrameGrabber
-import org.bytedeco.javacv.FFmpegFrameRecorder
+import org.bytedeco.javacpp.Loader
 import org.springframework.stereotype.Service
-import java.lang.Exception
-import java.util.concurrent.ExecutorService
 
 private val log = KotlinLogging.logger {}
 
-data class SRSValue(var recorder: FFmpegFrameRecorder, var grabber: FFmpegFrameGrabber)
-
 @Service
-class SRSService(private val srsCache: Cache<String, SRSValue>, private val processSrsThreadPoolExecutorService: ExecutorService, private val apiConfiguration: ApiConfiguration) {
+class SRSService(
+    private val srsCache: Cache<String, Process>,
+    private val apiConfiguration: ApiConfiguration
+) {
     fun publish(srsCallbackBody: SRSCallbackBody) {
-        processSrsThreadPoolExecutorService.submit {
-            process("${srsCallbackBody.tcUrl}/${srsCallbackBody.stream}",
-                    apiConfiguration.bilibiliRtmp, srsCache, srsCallbackBody.clientId)
-        }
+        val p = process(
+            "${srsCallbackBody.tcUrl}/${srsCallbackBody.stream}",
+            apiConfiguration.bilibiliRtmp
+        )
+        srsCache.put(srsCallbackBody.clientId, p)
     }
 
     fun unpublished(srsCallbackBody: SRSCallbackBody) {
         log.info(srsCallbackBody.clientId)
-        val srsValue = srsCache.getIfPresent(srsCallbackBody.clientId) ?: return
-        try {
-            srsValue.recorder.close()
-            srsValue.grabber.close()
-        } finally {
-            srsCache.invalidate(srsCallbackBody.clientId)
-        }
+        srsCache.invalidate(srsCallbackBody.clientId)
+    }
+
+    @PreDestroy
+    fun destory() {
+        srsCache.invalidateAll()
     }
 }
 
-fun process(from: String, to: String, srsCache: Cache<String, SRSValue>, key: String) {
-    log.info("from: $from, to: $to")
-    val grabber = FFmpegFrameGrabber(from)
-    grabber.start()
-    val recorder = FFmpegFrameRecorder(to, grabber.imageWidth, grabber.imageHeight, grabber.audioChannels)
-    recorder.format = "flv"
-    recorder.videoCodec = AV_CODEC_ID_H264;
-    recorder.audioCodec = AV_CODEC_ID_AAC;
-    recorder.start(grabber.formatContext)
-    srsCache.put(key, SRSValue(recorder, grabber))
-    try {
-        log.info("start record")
-        while (true) {
-            recorder.recordPacket(grabber.grabPacket())
-        }
-    } catch (e: Exception) {
-        log.warn("forward failed", e)
-    } finally {
-        log.info("close recorder and grabber")
-        recorder.close()
-        grabber.close()
-    }
+fun process(from: String, to: String): Process {
+    val ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg::class.java)
+    val processBuilder =
+        ProcessBuilder(ffmpeg, "-re", "-i", from, "-codec", "copy", "-f", "flv", "-y", to)
+    return processBuilder.inheritIO().start()
 }
