7dc7b6cce876e67eb3fc62d2ebfef07ef6bb8230
Victor <tonyyunhan1997@163.com>
2023-01-26 19:15:57 +0800

    update

--- src/main/kotlin/com/tang/victor/ssrffmpeg/SsrFfmpegApplication.kt
+++ src/main/kotlin/com/tang/victor/ssrffmpeg/SsrFfmpegApplication.kt
@@ -2,12 +2,10 @@ package com.tang.victor.ssrffmpeg
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
-@ComponentScan("com.tang.victor.ssrffmpeg")
 @SpringBootApplication
 class SsrFfmpegApplication
 
 fun main(args: Array<String>) {
-	runApplication<SsrFfmpegApplication>(*args)
+    runApplication<SsrFfmpegApplication>(*args)
 }
