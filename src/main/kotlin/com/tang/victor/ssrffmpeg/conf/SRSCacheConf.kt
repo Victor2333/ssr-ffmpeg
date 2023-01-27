a9c4af9e3ad3033c2908100798f5ddfc7d71bc2b
Victor <tonyyunhan1997@163.com>
2023-01-27 13:54:24 +0800

    add cache life

--- src/main/kotlin/com/tang/victor/ssrffmpeg/conf/SRSCacheConf.kt
+++ src/main/kotlin/com/tang/victor/ssrffmpeg/conf/SRSCacheConf.kt
@@ -2,16 +2,24 @@ package com.tang.victor.ssrffmpeg.conf
 
 import com.google.common.cache.Cache
 import com.google.common.cache.CacheBuilder
+import mu.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
+
+val log = KotlinLogging.logger { }
 
 @Configuration
 class SRSCacheConf {
     @Bean
     fun srsCache(): Cache<String, Process> {
         return CacheBuilder.newBuilder()
+            .expireAfterWrite(Duration.ofDays(1))
             .removalListener<String, Process> {
-                it.value?.destroy()
+                log.info { "Cache invalidate ${it.key} ${it.value?.pid()}" }
+                if (it.value?.isAlive == true) {
+                    it.value?.destroy()
+                }
             }
             .build()
     }
